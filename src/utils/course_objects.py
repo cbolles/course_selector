c3ad5c80ec40422865fb2b27d9248110928ecbc4
Collin Bolles <collinbolles@gmail.com>
2018-09-26 22:59:03 -0400

    Get basic course information

--- src/utils/course_objects.py
+++ src/utils/course_objects.py
@@ -1,4 +1,12 @@
-class ProgramType(enum.Enum):
+from enum import Enum
+
+
+class CourseType(Enum):
+    prereq = 'prerequisite'
+    elective = 'elective'
+
+
+class ProgramType(Enum):
     immersion = 'immersion'
     minor = 'minor'
     major = 'major'
@@ -11,6 +19,24 @@ class Course:
         self.name = name
         self.course_id = course_id
 
+    def __str__(self):
+        return 'Name: ' + self.name + ', ID: ' + self.course_id
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class CourseSequence:
+    __slots__ = ['courses', 'num_required']
+
+    def __init__(self):
+        self.courses = []
+        self.num_required = 0
+
+    def __init__(self, courses, num_required):
+        self.courses = courses
+        self.num_required = num_required
+
 
 class Program:
     __slots__ = ['name', 'program_type', 'prereqs', 'electives', 'notes']
@@ -18,7 +44,7 @@ class Program:
     def __init__(self, name, program_type):
         self.name = name
         self.program_type = program_type
-        self.prereqs = []
-        self.electives = []
+        self.preq = CourseSequence()
+        self.electives = CourseSequence()
 
 
--- src/utils/web_scrapper.py
+++ src/utils/web_scrapper.py
@@ -1,7 +1,7 @@
 import configparser
 from bs4 import BeautifulSoup
 import urllib.request
-import utils.course_objects
+from utils.course_objects import ProgramType, CourseType, Course
 
 
 def get_config(config_file: str) -> dict:
@@ -11,15 +11,46 @@ def get_config(config_file: str) -> dict:
 
 
 def get_program_data(config, program_name, program_type):
-    url = config['immersions']['immersions_site'] + immersion_name
+    if program_type == ProgramType.immersion:
+        url = config['immersions']['immersion_site'] + program_name
+    elif program_type == ProgramType.minor:
+        url = config['minor']['minor_site'] + program_name
+    else:
+        url = config['major']['major_site'] + program_name
+
     immersion_page = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(immersion_page)
-    print(soup.prettify())
+    soup = BeautifulSoup(immersion_page, features='html.parser')
+    course_body = soup.table.tbody
+
+    prereq_courses = []
+    elective_courses = []
+    course_type = CourseType.prereq
+    for raw_course in course_body.find_all('tr'):
+        course_details = raw_course.find_all('td')
+        if len(course_details) > 1:
+            course_id = course_details[0].string
+            course_name = course_details[1].string
+            if course_type == CourseType.prereq:
+                prereq_courses.append(Course(course_name, course_id))
+            elif course_type == CourseType.elective:
+                elective_courses.append(Course(course_name, course_id))
+        elif len(course_details) > 0 and CourseType.elective.value in course_details[0].string.lower():
+            print(course_details)
+            course_type = CourseType.elective
+
+    print(prereq_courses)
+    print(elective_courses)
+
+    # print(raw_course_details.prettify())
 
 
 def test():
     config = get_config('../properties/web_details.ini')
+    get_program_data(config, 'mathematics', ProgramType.immersion)
 
+
+if __name__ == '__main__':
+    test()
 
 
 
